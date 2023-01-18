5bf19ff08ed30bb07357219e9ee8e778276d1caa
jelitaa6 <jelita.firdaussakinah.tik20@mhsw.pnj.ac.id>
2023-01-19 06:01:27 +0700

    history back

--- app/src/main/java/com/example/medicare/ActivityHistory.java
+++ app/src/main/java/com/example/medicare/ActivityHistory.java
@@ -2,22 +2,27 @@ package com.example.medicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ActivityHistory extends AppCompatActivity {
 
-    ImageView imageView;
-    TextView mjudul, mharga, mper;
-    String xjudul = "judul";
-    String xharga = "harga";
-    String xper = "per";
-    String gambar = "gambar";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        ImageButton backhome = findViewById(R.id.btn_backhome);
+        backhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityHistory.this, dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
